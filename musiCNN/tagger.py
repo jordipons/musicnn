2a1f683b6d3658eb2df6091d79d14b816d9e162a
jordipons <jordipons@users.noreply.github.com>
2019-07-11 08:01:20 +0200

    Update tagger.py

--- musiCNN/tagger.py
+++ musiCNN/tagger.py
@@ -2,10 +2,15 @@ import numpy as np
 from musiCNN.extractor import extractor
 
 
-def top_tags(file_name, model='MTT', topN=3, input_length=3, input_overlap=None):
+def top_tags(file_name, model='MTT', topN=3, input_length=3, input_overlap=None, verbose=1):
     # TODO: document
 
     taggram, tags = extractor(file_name, model=model, input_length=input_length, input_overlap=input_overlap)
     tags_likelihood_mean = np.mean(taggram, axis=0)
-    for tag_index in tags_likelihood_mean.argsort()[-topN:][::-1]:
-        print(' - ' + tags[tag_index])
+    tags = tags_likelihood_mean.argsort()[-topN:][::-1]
+    
+    if verbose=1:
+        for tag_index in tags_likelihood_mean.argsort()[-topN:][::-1]:
+            print(' - ' + tags[tag_index])
+            
+    return tags
