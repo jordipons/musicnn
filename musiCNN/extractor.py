89143b2c930404ffb591c6800e0f9c55dbe131cd
jordipons <jordipons@users.noreply.github.com>
2019-07-12 12:02:44 +0200

    Update extractor.py

--- musiCNN/extractor.py
+++ musiCNN/extractor.py
@@ -35,8 +35,52 @@ def batch_data(audio_file, n_frames, overlap):
 
 
 def extractor(file_name, model='MTT', input_length=3, input_overlap=False, extract_features=False):
-    # TODO: documentation
+    '''Extract the taggram (the temporal evolution of tags) and features (intermediate representations of the model) of the music-clip in file_name with the selected model.
 
+    INPUT
+
+    - file_name: path to the music file to tag.
+    Data format: string.
+    Example: './audio/TRWJAZW128F42760DD_test.mp3'
+    
+    - model: select the music audio tagging model.
+    Data format: string.
+    Options: 'MTT' (model trained with the MagnaTagATune dataset). To know more about our this model, check our advanced example and FAQs.
+    
+    - topN: extract N most likely tags according to the selected model.
+    Data format: integer.
+    Example: 3
+    
+    - input_length: length (in seconds) of the input spectrogram patches. Set it small for real-time applications.
+    This is the length of the data that is going to be fed to the model. In other words, this parameter defines the temporal resolution of the taggram. Check our basic / advanced examples to know more about that.
+    Data format: floating point number.
+    Example: 3.1
+    
+    - input_overlap: ammount of overlap (in seconds) of the input spectrogram patches.
+    Note: Set it considering the input_length.
+    Data format: floating point number.
+    Example: 1
+    
+    - extract_features: set it True for extracting the intermediate representations of the model.
+    Data format: True or False (boolean).
+    Options: False (for NOT extracting the features), True (for extracting the features).
+
+    OUTPUT
+
+    - taggram: expresses the temporal evolution of the tags likelihood.
+    Data format: 2D np.ndarray (time, tags).
+    Example: see our basic / advanced examples.
+    
+    - tags: list of tags corresponding to the tag-indices of the taggram.
+    Data format: list.
+    Example: see our FAQs page for the complete tags list.
+    
+    - features: a dictionary containing the outputs of the different layers that the selected model has.
+    Data format: dictionary.
+    Keys: ['timbral', 'temporal', 'cnn1', 'cnn2', 'cnn3', 'mean_pool', 'max_pool', 'penultimate']
+    Example: see our advanced examples.
+    '''
+    
     # select model
     if model == 'MTT':
         labels = config.MTT_LABELS
