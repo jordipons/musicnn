ee1be04d4dcd1fe31fb0081f52a69bc3881cdf91
jordipons <jordipons@users.noreply.github.com>
2019-07-12 10:27:45 +0200

    Update tagger.py

--- musiCNN/tagger.py
+++ musiCNN/tagger.py
@@ -2,7 +2,7 @@ import numpy as np
 from musiCNN.extractor import extractor
 
 
-def top_tags(file_name, model='MTT', topN=3, input_length=3, input_overlap=None, print_tags=True):
+def top_tags(file_name, model='MTT', topN=3, input_length=3, input_overlap=False, print_tags=True):
     # TODO: document
 
     taggram, tags = extractor(file_name, model=model, input_length=input_length, input_overlap=input_overlap)
