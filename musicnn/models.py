98b61a68b517c05c017bcd39a6aa397a72431420
jordipons <jordipons@users.noreply.github.com>
2019-08-01 07:29:32 -0700

    Update models.py

--- musicnn/models.py
+++ musicnn/models.py
@@ -192,65 +192,65 @@ def backend(feature_map, is_training, num_classes, output_units, type):
 
 def vgg(x, is_training, num_classes, num_filters=32):
     input_layer = tf.expand_dims(x, 3)
-    bn_input = tf.layers.batch_normalization(input_layer, training=is_training)
+    bn_input = tf.compat.v1.layers.batch_normalization(input_layer, training=is_training)
 
-    conv1 = tf.layers.conv2d(inputs=bn_input,
+    conv1 = tf.compat.v1.layers.conv2d(inputs=bn_input,
                              filters=num_filters,
                              kernel_size=[3, 3],
                              padding='same',
                              activation=tf.nn.relu,
                              name='1CNN',
                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
-    bn_conv1 = tf.layers.batch_normalization(conv1, training=is_training)
-    pool1 = tf.layers.max_pooling2d(inputs=bn_conv1, pool_size=[4, 1], strides=[2, 2])
+    bn_conv1 = tf.compat.v1.layers.batch_normalization(conv1, training=is_training)
+    pool1 = tf.compat.v1.layers.max_pooling2d(inputs=bn_conv1, pool_size=[4, 1], strides=[2, 2])
 
-    do_pool1 = tf.layers.dropout(pool1, rate=0.25, training=is_training)
-    conv2 = tf.layers.conv2d(inputs=do_pool1,
+    do_pool1 = tf.compat.v1.layers.dropout(pool1, rate=0.25, training=is_training)
+    conv2 = tf.compat.v1.layers.conv2d(inputs=do_pool1,
                              filters=num_filters,
                              kernel_size=[3, 3],
                              padding='same',
                              activation=tf.nn.relu,
                              name='2CNN',
                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
-    bn_conv2 = tf.layers.batch_normalization(conv2, training=is_training)
-    pool2 = tf.layers.max_pooling2d(inputs=bn_conv2, pool_size=[2, 2], strides=[2, 2])
+    bn_conv2 = tf.compat.v1.layers.batch_normalization(conv2, training=is_training)
+    pool2 = tf.compat.v1.layers.max_pooling2d(inputs=bn_conv2, pool_size=[2, 2], strides=[2, 2])
 
-    do_pool2 = tf.layers.dropout(pool2, rate=0.25, training=is_training)
-    conv3 = tf.layers.conv2d(inputs=do_pool2,
+    do_pool2 = tf.compat.v1.layers.dropout(pool2, rate=0.25, training=is_training)
+    conv3 = tf.compat.v1.layers.conv2d(inputs=do_pool2,
                              filters=num_filters,
                              kernel_size=[3, 3],
                              padding='same',
                              activation=tf.nn.relu,
                              name='3CNN',
                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
-    bn_conv3 = tf.layers.batch_normalization(conv3, training=is_training)
-    pool3 = tf.layers.max_pooling2d(inputs=bn_conv3, pool_size=[2, 2], strides=[2, 2])
+    bn_conv3 = tf.compat.v1.layers.batch_normalization(conv3, training=is_training)
+    pool3 = tf.compat.v1.layers.max_pooling2d(inputs=bn_conv3, pool_size=[2, 2], strides=[2, 2])
 
-    do_pool3 = tf.layers.dropout(pool3, rate=0.25, training=is_training)
-    conv4 = tf.layers.conv2d(inputs=do_pool3,
+    do_pool3 = tf.compat.v1.layers.dropout(pool3, rate=0.25, training=is_training)
+    conv4 = tf.compat.v1.layers.conv2d(inputs=do_pool3,
                              filters=num_filters,
                              kernel_size=[3, 3],
                              padding='same',
                              activation=tf.nn.relu,
                              name='4CNN',
                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
-    bn_conv4 = tf.layers.batch_normalization(conv4, training=is_training)
-    pool4 = tf.layers.max_pooling2d(inputs=bn_conv4, pool_size=[2, 2], strides=[2, 2])
+    bn_conv4 = tf.compat.v1.layers.batch_normalization(conv4, training=is_training)
+    pool4 = tf.compat.v1.layers.max_pooling2d(inputs=bn_conv4, pool_size=[2, 2], strides=[2, 2])
 
-    do_pool4 = tf.layers.dropout(pool4, rate=0.25, training=is_training)
-    conv5 = tf.layers.conv2d(inputs=do_pool4, 
+    do_pool4 = tf.compat.v1.layers.dropout(pool4, rate=0.25, training=is_training)
+    conv5 = tf.compat.v1.layers.conv2d(inputs=do_pool4, 
                              filters=num_filters, 
                              kernel_size=[3, 3], 
                              padding='same', 
                              activation=tf.nn.relu,
                              name='5CNN', 
                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
-    bn_conv5 = tf.layers.batch_normalization(conv5, training=is_training)
-    pool5 = tf.layers.max_pooling2d(inputs=bn_conv5, pool_size=[4, 4], strides=[4, 4])
+    bn_conv5 = tf.compat.v1.layers.batch_normalization(conv5, training=is_training)
+    pool5 = tf.compat.v1.layers.max_pooling2d(inputs=bn_conv5, pool_size=[4, 4], strides=[4, 4])
 
     flat_pool5 = tf.contrib.layers.flatten(pool5)
-    do_pool5 = tf.layers.dropout(flat_pool5, rate=0.5, training=is_training)
-    output = tf.layers.dense(inputs=do_pool5,
+    do_pool5 = tf.compat.v1.layers.dropout(flat_pool5, rate=0.5, training=is_training)
+    output = tf.compat.v1.layers.dense(inputs=do_pool5,
                             activation=None,
                             units=num_classes,
                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
