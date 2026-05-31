9470bf0a13bbe28c38d08435a7e0fccc5fcb2fec
sajmon <szymon17099@gmail.com>
2026-05-31 21:13:19 +0200

    UV me harder

--- shell.nix
+++ shell.nix
@@ -2,10 +2,27 @@
 
 nixpkgs.mkShell {
   nativeBuildInputs = with nixpkgs; [
-     uv
+    uv
+    glib
+    zlib
+    libGL
+    fontconfig
+    xorg.libX11
+    libxkbcommon
+    freetype
+    dbus
+
+    qt6.qtbase
+    qt6.qtwayland
+
+    xorg.libxcb
+    xorg.xcbutil
+    xorg.xcbutilwm
+    xorg.xcbutilimage
+    xorg.xcbutilkeysyms
   ];
   shellHook = ''
     export CUDA_PATH=${nixpkgs.cudatoolkit}
-    export LD_LIBRARY_PATH=${nixpkgs.stdenv.cc.cc.lib}
+    export LD_LIBRARY_PATH=/run/opengl-driver/lib:${nixpkgs.cudaPackages.cudnn.lib}:${nixpkgs.cudatoolkit.lib}:${nixpkgs.stdenv.cc.cc.lib}:$LD_LIBRARY_PATH
   '';
 }
